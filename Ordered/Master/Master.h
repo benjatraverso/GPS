f0877ad4aed17f038ebbaa20ebc58065ce25e343
benjatraverso <benjatraverso@gmail.com>
2016-11-16 16:53:47 -0300

    new master version

--- Ordered/Master/Master.h
+++ Ordered/Master/Master.h
@@ -1,5 +1,11 @@
 #include <Wire.h>
 
 const int SLAVE_ADDRESS = 8;
-const int DATA_LENGTH = 8*2+2+3;//8char data twice plus 2 chars "sign" + 3 separating commas
-String gsBuffer = "";
+const int DATA_LENGTH = sizeof("DSAAAA.AA,SOOOOO.OO*");
+
+bool gbNewData = false;
+float gfLat;
+float gfLon;
+
+String gfBuffer = "";
+
--- Ordered/Master/Master.ino
+++ Ordered/Master/Master.ino
@@ -1,27 +1,63 @@
+#include "Master.h"
+
 void setup()
 {
   Wire.begin();        // join i2c bus (address optional for master)
   Serial.begin(9600);  // start serial for output
-  gsBuffer.reserve(50);
+  gfBuffer.reserve( DATA_LENGTH );
 }
 
 void loop()
 {
-  Wire.requestFrom(SLAVE_ADDRESS, DATA_LENGTH);    // request 6 bytes from slave device #8
+  bool bNewData = false;
+  Wire.requestFrom(SLAVE_ADDRESS, DATA_LENGTH);
 
-  int deg = Wire.read();
-  while( Wire.available() )
+  if( Wire.available() )
   {
-    gsBuffer =+ Wire.read(); // receive a byte as character
+    bNewData = (bool)Wire.read();
   }
 
-  printBuffer();
-  gsBuffer = "";
+  if( bNewData )
+  {
+    gsBuffer = "";
+    while( Wire.available() )
+    {
+      gsBuffer += (char)Wire.read(); // receive a byte as character
+    }
+  }
 
-  delay(1000);
-}
+  if( bNewData )
+  {
+    //00000000001111111111
+    //01234567890123456789
+    //DSAAAA.AA,SOOOOO.OO*
+    int   iLatDeg = gsBuffer.substring( 2, 4 ).toInt();
+    float fLatMin = gsBuffer.substring( 4, 9 ).toFloat();
+    int   iLongDeg = gsBuffer.substring( 11, 14 ).toInt();
+    float fLongMin = gsBuffer.substring( 14, 19 ).toFloat();
+    gfLat = ( float )iLatDeg + fLatMin / 60.0;
+    gfLon = ( float )iLongDeg + fLatgMin / 60.0;
+    
+    if( 'S' == gsBuffer.charAt(0) )
+    {
+      gfLat *= -1;
+    }
 
-void printBuffer(void)
-{
-	Serial.println(gsBuffer);
+    if( 'W' == gsBuffer.charAt(10) )
+    {
+      gfLon *= -1;
+    }
+    
+    Serial.print( "Data: " );
+    Serial.print( "Latitude " );
+    Serial.print( gfLat, 4 );
+    Serial.print( "Longitude " );
+    Serial.println( gfLon, 4 );
+    gbNewData = false;
+  }
+  else
+  {
+    Serial.println( "Empty new package" );
+  }
+  delay( 5000 );
 }
